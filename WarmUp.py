f0cbc95ba1c049a78d72e5138324e93722379e34
Matt Roth <mrothhb@gmail.com>
2019-07-14 16:32:01 -0700

    Soup stuff

--- WarmUp.py
+++ WarmUp.py
@@ -1,2 +1,21 @@
-HELLO
-#Filler comment
+from bs4 import BeautifulSoup
+
+import urllib.request
+from urllib.request import urlopen
+import requests
+
+# setup request 
+url = "https://www.coinbase.com/price"
+
+#Make request
+req2 = requests.get(url)
+
+#Obtain html string for requested url
+htmlstring2 = req2.text
+
+#Use BeautifulSoup to parse html string
+soup = BeautifulSoup(htmlstring2, 'html.parser')
+
+#print(soup.prettify())
+
+print(soup.title)
