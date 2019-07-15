a2c4c2db7c0cdad6b8a5ed1857a3d29b36dc1587
Matt Roth <mrothhb@gmail.com>
2019-07-14 17:02:20 -0700

    API Bitcoin price

--- WarmUp.py
+++ WarmUp.py
@@ -3,19 +3,36 @@ from bs4 import BeautifulSoup
 import urllib.request
 from urllib.request import urlopen
 import requests
+import json
+import time
 
 # setup request 
-url = "https://www.coinbase.com/price"
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
-#Make request
-req2 = requests.get(url)
+while True:
+
+    #Make request
+    priceReq = urlopen(url)
+
+    #Read JSON from API request
+    priceJSON = json.load(priceReq)
+
+    rate = priceJSON["bpi"]["USD"]["rate"]
+
+    print("$" + rate)
+    time.sleep(10)
+
+
+
+#Soup stuff
+#req2 = requests.get(url)
 
 #Obtain html string for requested url
-htmlstring2 = req2.text
+#htmlstring2 = req2.text
 
 #Use BeautifulSoup to parse html string
-soup = BeautifulSoup(htmlstring2, 'html.parser')
+#soup = BeautifulSoup(htmlstring2, 'html.parser')
 
 #print(soup.prettify())
 
-print(soup.title)
+#print(soup.title)
